c94ea1e9abbc90dd3bd108ac584ef3736ddf102b
Ashish Malla <ashishmalla45@gmail.com>
2023-01-06 19:26:14 -0700

    render header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
+import Header from './components/Header'
+
 const App = () => {
 	return (
-		<h1 className='text-3xl font-bold underline text-blue-900'>Hello world!</h1>
+		<>
+			<Header />
+		</>
 	)
 }
 
