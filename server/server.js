ac4bc9abcf16153af98be23aa5494b5455eb3141
Ashish Malla <ashishmalla45@gmail.com>
2023-01-07 20:09:43 -0700

    remove the use of colors

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import router from './router.js'
+import router from './routes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+//middleware - requests we make before we make any request
+app.use(express.json()) //need this middleware to process JSON request
+app.use(cors())
 
 app.get('/', (req, res) => {
 	res.send('API is running...')
 })
-
-//middleware - requests we make before we make any request
-app.use(express.json()) //need this middleware to process JSON request
-app.use(cors())
 
 app.use(router)
 
@@ -29,6 +29,6 @@ const PORT = process.env.PORT || 5000
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+		`Server running in ${process.env.NODE_ENV} on port ${PORT}`
 	)
 )
