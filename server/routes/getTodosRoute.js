9190b268b05e1223deb69dd6a619fdac5f3f7424
Ashish Malla <ashishmalla45@gmail.com>
2023-01-07 19:50:41 -0700

    add a route to get all the todos

---
+++ server/routes/getTodosRoute.js
@@ -0,0 +1,12 @@
+import TodoModel from '../models/TodoModel'
+
+const getTodos = async (req, res) => {
+	try {
+		const todos = await TodoModel.find()
+		res.json(todos)
+	} catch (error) {
+		res.status(500).json({ message: error.message })
+	}
+}
+
+export default getTodos
