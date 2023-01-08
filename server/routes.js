af34ba901c012e2675ee4dc601fffb64a2e84d66
Ashish Malla <ashishmalla45@gmail.com>
2023-01-07 19:48:57 -0700

    connect to all the todo routes

---
+++ server/routes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import getTodos from './routes/getTodosRoute'
+import createTodo from './routes/createTodoRoute'
+import deleteTodo from './routes/deleteTodoRoute'
+import { updateTextTodo, updateIsCompleteTodo } from './routes/updateTodoRoute'
+
+const router = express.Router()
+
+router.get('/todos', getTodos)
+router.post('/todos', createTodo)
+router.delete('/todos/:id', deleteTodo)
+router.patch('/todos/:id', updateTextTodo)
+router.patch('/todos/:id', updateIsCompleteTodo)
+
+export default router
