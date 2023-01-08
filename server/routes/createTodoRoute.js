27a99007d52f8133e1c28a80f39e38232ab75338
Ashish Malla <ashishmalla45@gmail.com>
2023-01-07 19:50:26 -0700

    add a route to create a new todo

---
+++ server/routes/createTodoRoute.js
@@ -0,0 +1,15 @@
+import TodoModel from '../models/TodoModel'
+
+const createTodo = async (req, res) => {
+	const todo = new TodoModel({
+		text: req.body.text,
+	})
+	try {
+		const newTodo = await todo.save()
+		res.status(201).json(newTodo)
+	} catch (error) {
+		res.status(400).json({ message: error.message })
+	}
+}
+
+export default createTodo
