a9e9ad4cd30d923f3569ccc9085aee60d1511e04
Ashish Malla <ashishmalla45@gmail.com>
2023-01-07 19:50:52 -0700

    add a route to update the text and isComplete field

---
+++ server/routes/updateTodoRoute.js
@@ -0,0 +1,25 @@
+import TodoModel from '../models/TodoModel'
+
+export const updateTextTodo = async (req, res) => {
+	try {
+		const todo = await TodoModel.findById(req.params.id)
+		if (!todo) return res.status(404).json({ message: 'Cannot find todo' })
+		todo.text = req.body.text
+    await todo.save()
+		res.json(todo)
+	} catch (error) {
+		res.status(400).json({ message: error.message })
+	}
+}
+
+export const updateIsCompleteTodo = async (req, res) => {
+	try {
+		const todo = await TodoModel.findById(req.params.id)
+		if (!todo) return res.status(404).json({ message: 'Cannot find todo' })
+		todo.isComplete = !todo.isComplete
+		await todo.save()
+		res.json(todo)
+	} catch (error) {
+		res.status(400).json({ message: error.message })
+	}
+}
